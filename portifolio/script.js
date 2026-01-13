95009e935dffacdfd9492123bc8f04b39a5477b0
srideviucd22044 <srid22044.cd@rmkec.ac.in>
2026-01-13 11:59:11 +0530

    Initial commit

---
+++ portifolio/script.js
@@ -0,0 +1,29 @@
+
+var typed = new Typed(".text", {
+    strings: ["B.E-Computer Science and Design"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
+/*
+function send(){
+    const form = document.getElementById("form");
+    let nam = document.forms["form"]["name"].value;
+    let mail = document.forms["form"]["mail"].value;
+    let sub = document.forms["form"]["sub"].value;
+    let msg = document.forms["form"]["msg"].value;
+    console.log(nam);
+}*/
+
+
+const toTop = document.querySelector(".top");
+window.addEventListener("scroll",() =>{
+    if (window.pageYOffset > 100){
+        toTop.classList.add("active");
+    }
+    else{
+        toTop.classList.remove("active");
+    }
+})
